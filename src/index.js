657f958109c231ef933085e0c3482edf484e62fa
ViliKo <59921526+ViliKo@users.noreply.github.com>
2021-10-20 15:39:43 +0300

    This is wrong I have to update again

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const img = "https://picsum.photos/200";
+
 ReactDOM.render(
   <div>
-    <h1 className="heading" contentEditable="true" spellCheck="false">
-      My Favourite Foods
-    </h1>
+    <h1 className="heading">My Favourite Foods</h1>
     <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
+      <li>
+        <img src={img} alt="home" />
+        <img src={img} alt="home" />
+        <img src={img} alt="home" />
+      </li>
     </ul>
   </div>,
   document.getElementById("root")
